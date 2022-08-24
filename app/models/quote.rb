bbb3529f7537bfc83f53b185bdf394cc35d93889
fugakkbn <aax.chiri@gmail.com>
2022-08-24 23:54:26 +0900

    見積りの作成・更新・削除の非同期実行をリファクタリング

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -3,10 +3,9 @@ class Quote < ApplicationRecord
 
   scope :ordered, -> { order(id: :desc) }
 
-  after_create_commit -> { broadcast_prepend_later_to 'quotes' }
-  after_update_commit -> { broadcast_replace_later_to 'quotes' }
-  # broadcast_remove_later_to はない
-  # Jobが実行される前に当該Quoteが削除されてしまうと、
-  # JobでQuoteを見つけることができなくなってしまうため
-  after_destroy_commit -> { broadcast_remove_to 'quotes' }
+  # after_create_commit -> { broadcast_prepend_later_to 'quotes' }
+  # after_update_commit -> { broadcast_replace_later_to 'quotes' }
+  # after_destroy_commit -> { broadcast_remove_to 'quotes' }
+  #   これらは1行で以下のように書ける
+  broadcasts_to -> (quote) { 'quotes' }, inserts_by: :prepend
 end
